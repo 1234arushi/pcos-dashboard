873d4fd3e8f844a44f9ca24cb402a73769140213
Arushi Sharma <arushi@mail.busy.in>
2025-09-21 14:13:48 +0530

    implementing in ETL form

---
+++ src/load.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine,text
+from pathlib import Path
+
+CLEANED=Path(__file__).resolve().parents[1]/"outputs"/"pcos_cleaned.csv"
+
+#DB connection details
+DB_USER="postgres"
+DB_PASS="1234"
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_NAME="pcos_db"
+
+#build connection string for sqlAlchemy
+CONN_STR= f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+def load_to_postgres():
+    df=pd.read_csv(CLEANED)
+    print("Loaded cleaned data with shape : ",df.shape)
+
+    #create engine
+    engine=create_engine(CONN_STR)
+
+    #load into postgres table(replace if exists,otherwise create)
+    df.to_sql("patients",engine,if_exists="replace",index=False)
+
+    with engine.connect() as conn:
+        conn.execute(text("""
+        ALTER TABLE patients
+        ADD COLUMN id SERIAL PRIMARY KEY;
+        
+        """))
+        conn.commit()
+    print(f"Loaded {len(df)} rows into 'patients' table in db")
+
+if __name__=="__main__":
+    load_to_postgres()
+    
+
---
+++ src/transform.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+RAW = Path(__file__).resolve().parents[1]/"data"/"PCOS_data.csv"
+CLEANED = Path(__file__).resolve().parents[1]/"outputs"/"pcos_cleaned.csv"
+
+def transform():
+    df=pd.read_csv(RAW)
+    print("Loaded raw data: ",RAW,"with shape: ",df.shape)
+
+    df.columns=df.columns.str.strip()
+
+
+    #1. handling null values for text datatypes cols
+    for col in df.select_dtypes(include="object").columns:
+        mode_val=df[col].mode()[0]
+        df[col]=df[col].fillna(mode_val)
+    #2. handling null values for numeric datatype cols
+    for col in df.select_dtypes(include=[np.number]).columns:
+        median_val=df[col].median()
+        df[col]=df[col].fillna(median_val)
+
+    df = df.drop(columns=["Hip(inch)","Waist:Hip Ratio",
+    "TSH (mIU/L)", "AMH(ng/mL)", "PRL(ng/mL)", "PRG(ng/mL)", "RBS(mg/dl)",
+    "BP _Systolic (mmHg)", "BP _Diastolic (mmHg)", "Follicle No. (L)", 
+    "Follicle No. (R)", "Avg. F size (L) (mm)", "Avg. F size (R) (mm)", 
+    "Endometrium (mm)", "Sl. No", "Patient File No.", "Pulse rate(bpm)", 
+    "RR (breaths/min)", "Hb(g/dl)", "No. of abortions", "Unnamed: 44",
+    "I   beta-HCG(mIU/mL)", "II    beta-HCG(mIU/mL)", "FSH(mIU/mL)","LH(mIU/mL)","FSH/LH"], errors="ignore")
+   
+    df["Cycle(R/I)"]=df["Cycle(R/I)"].replace({2:0,4:1,5:1})
+   
+
+    df =df.rename(columns={
+   
+    "PCOS (Y/N)": "PCOS",
+    "Age (yrs)": "Age",
+    "Weight (Kg)": "Weight",
+    "Height(Cm)": "Height",
+    "BMI": "BMI",
+    "Blood Group": "Blood_Group",
+    "Cycle(R/I)": "Cycle_Type",
+    "Cycle length(days)": "Cycle_Length",
+    "Marraige Status (Yrs)": "Marriage_Years",
+    "Pregnant(Y/N)": "Pregnant",
+    "Waist(inch)": "Waist",
+    "Vit D3 (ng/mL)": "VitD3",
+    "Weight gain(Y/N)": "Weight_Gain",
+    "hair growth(Y/N)": "Hair_Growth",
+    "Skin darkening (Y/N)": "Skin_Darkening",
+    "Hair loss(Y/N)": "Hair_Loss",
+    "Pimples(Y/N)": "Pimples",
+    "Fast food (Y/N)": "Fast_Food",
+    "Reg.Exercise(Y/N)": "Exercise"
+
+
+   })
+    df["bmi_category"]=pd.cut(df["BMI"],bins=[0,18.5,24.9,29.9,100],labels=["underweight","normal","overweight","obese"])
+
+    #save the clean dataset
+    #parents = True->create any missing parent folders too
+    #exists = True->don't throw an error if the folder already exists
+    CLEANED.parent.mkdir(parents=True,exist_ok=True)
+    df.to_csv(CLEANED,index=False)#index=false->so that df index is not added as a separate col in csv
+    print("\n Columns after cleaning: ",list(df.columns))
+    print("\n First 5 rows: \n",df.head())
+  
+    
+    return df#useful if you use transform() in another script
+
+#calling as a standalone function
+if __name__=="__main__":
+    transform()
+
